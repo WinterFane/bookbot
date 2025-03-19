cdce0ba2424912881c70c6cda30c9dc1bb1a2d45
WinterFane <redveil629@gmail.com>
2025-03-20 00:29:58 +0800

    c2l5

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ def get_book_text(filepath):
         _str = file.read()
 
     return _str
+    #return ''
 
 def count_words(text):
-    
+    num = len(text.split())
+    _str = f"{num} words found in the document"
 
-    return 0
+    return _str
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    book = get_book_text("books/frankenstein.txt")
+    print(book)
+    print(count_words(book))
 
 
-
+#print("asd asd asd".split())
 main()
