e6b8f70596474538ac698051799a6c309a601140
WinterFane <redveil629@gmail.com>
2025-03-20 09:58:05 +0800

    c3l2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words,count_characters
+from stats import count_words,count_characters,to_sort
 
 def get_book_text(filepath):
     _str = ""
@@ -10,10 +10,21 @@ def get_book_text(filepath):
     #return ''
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
-    print(book)
+    filepath = "books/frankenstein.txt";
+
+    book = get_book_text(filepath)
+    #print(book)
+    
+    #print(count_characters(book))
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
     print(count_words(book))
-    print(count_characters(book))
+    print("--------- Character Count -------")
+    to_print = to_sort(count_characters(book))
+    for entry in to_print:
+        print(f"{entry["char"]}: {entry["count"]}")
 
 
 #print("asd asd asd".split())
--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def count_words(text):
     num = len(text.split())
-    _str = f"{num} words found in the document"
+    _str = f"Found {num} total words"#f"{num} words found in the document"
 
     return _str
 
@@ -9,9 +9,29 @@ def count_characters(text):
 
     for char in text:
         low_char = char.lower()
-        if(low_char not in char_dict):
+        if(char.isalpha() == False):
+            pass
+        elif(low_char not in char_dict):
             char_dict[low_char] = 1
         else:
             char_dict[low_char] += 1
         
     return char_dict
+
+def sort_on(dict):
+    return dict["count"]
+
+def to_sort(dict):
+    new_list = []
+
+    for entry in dict:
+        new_dict ={}
+        new_dict["char"] = entry
+        new_dict["count"] = dict[entry]
+
+        new_list.append(new_dict)
+
+    new_list.sort(reverse=True, key=sort_on)
+
+    #print(new_list)
+    return new_list
